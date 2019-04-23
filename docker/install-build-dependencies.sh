33a8eb78cbb3eaaf899a98a33f6b8f3d66a3477f
Taylor Hornby <taylor@defuse.ca>
2019-04-23 13:59:20 -0600

    Setup android emulator

--- docker/install-build-dependencies.sh
+++ docker/install-build-dependencies.sh
@@ -62,3 +62,12 @@ yes | "$ANDROID_HOME/tools/bin/sdkmanager" --licenses
 
 # Android NDK
 "$ANDROID_HOME/tools/bin/sdkmanager" "ndk-bundle"
+
+# Android Emulator
+===================================================
+
+$ANDROID_HOME/tools/bin/sdkmanager "platform-tools" "platforms;android-24" "emulator"
+$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-24;default;x86"
+yes no | head -n 1 | $ANDROID_HOME/tools/bin/avdmanager create avd -n zemu -k "system-images;android-24;default;x86"
+apt-get install -y tightvncserver
+echo 'export USER=root' >> /root/.bashrc
