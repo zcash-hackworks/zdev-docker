de5ce9fc9ccc532927661d96b1f961b956954058
Taylor Hornby <taylor@defuse.ca>
2019-10-08 13:33:36 -0600

    Add debugging tools and enable core dumps.

--- docker/install-basics.sh
+++ docker/install-basics.sh
@@ -7,3 +7,9 @@ sed -i 's/# \(.*multiverse$\)/\1/g' /etc/apt/sources.list
 apt-get update
 apt-get -y upgrade
 apt-get install -y software-properties-common curl git htop man unzip vim wget ack net-tools
+# Debugging tools
+apt-get install -y gdb strace tcpdump linux-perf xxd bsdmainutils
+
+# Shell customizations.
+# Enable core dumps.
+echo "ulimit -c unlimited" >> /root/.bashrc
